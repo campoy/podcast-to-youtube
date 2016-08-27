85243ea1f9c4f65e1f6aa4403cc1bac65806780c
Francesc Campoy <campoy@google.com>
2016-08-26 17:35:38 -0700

    fixing stuff

--- main.go
+++ main.go
@@ -1,16 +1,3 @@
-// Copyright 2016 Google Inc. All rights reserved.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to writing, software distributed
-// under the License is distributed on a "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied.
-//
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 // The podcast2youtube command uses ffmpeg to generate videos from any given
 // podcast, by downloading the mp3 and adding a fix image with a given logo
 // and text.
@@ -19,12 +6,14 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"image"
 	"image/png"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
@@ -36,8 +25,8 @@ var (
 	rssFeed   = flag.String("rss", "http://feeds.feedburner.com/GcpPodcast?format=xml", "url for the RSS feed")
 	logo      = flag.String("logo", "logo.png", "path to the PNG logo image")
 	titleTmpl = flag.String("title", "%s: GCPPodcast %d", "template used for the title")
-	fgHex     = flag.String("fg", "009688", "hex encoded color for the video text")
-	bgHex     = flag.String("bg", "ffffff", "hex encoded color for the video background")
+	fgHex     = flag.String("fg", "ffffff", "hex encoded color for the video text")
+	bgHex     = flag.String("bg", "009688", "hex encoded color for the video background")
 	width     = flag.Int("w", 1200, "width of the generated video in pixels")
 	height    = flag.Int("h", 800, "height of the generated video in pixels")
 )
@@ -64,13 +53,65 @@ func main() {
 	}
 
 	ctx := context.Background()
-	if err := buildAndUpload(ctx, *logo, ep); err != nil {
+	if err := buildAndUpload(ctx, ep); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func buildAndUpload(ctx context.Context, logo string, ep *episode) error {
+type episode struct {
+	Title  string
+	Number int
+	Link   string
+	Desc   string
+	MP3    string
+	Tags   []string
+}
+
+func fetchEpisode(rss string, number int) (*episode, error) {
+	res, err := http.Get(rss)
+	if err != nil {
+		return nil, fmt.Errorf("could not get %s: %v", rss, err)
+	}
+	defer func() { _ = res.Body.Close() }()
+
+	var data struct {
+		XMLName xml.Name `xml:"rss"`
+		Channel []struct {
+			Item []struct {
+				Title  string `xml:"title"`
+				Number int    `xml:"order"`
+				Link   string `xml:"guid"`
+				Desc   string `xml:"summary"`
+				MP3    struct {
+					URL string `xml:"url,attr"`
+				} `xml:"enclosure"`
+				Category []string `xml:"category"`
+			} `xml:"item"`
+		} `xml:"channel"`
+	}
+
+	if err := xml.NewDecoder(res.Body).Decode(&data); err != nil {
+		return nil, fmt.Errorf("could not decode feed: %v", err)
+	}
+
+	for _, i := range data.Channel[0].Item {
+		if i.Number == number {
+			return &episode{
+				Title:  i.Title,
+				Number: i.Number,
+				Link:   i.Link,
+				Desc:   i.Desc,
+				MP3:    i.MP3.URL,
+				Tags:   i.Category,
+			}, nil
+		}
+	}
+
+	return nil, fmt.Errorf("could not find episode %d", number)
+}
+
+func buildAndUpload(ctx context.Context, ep *episode) error {
 	tmpDir, err := ioutil.TempDir("", "")
 	if err != nil {
 		return fmt.Errorf("could not create temp directory: %v", err)
@@ -81,9 +122,9 @@ func buildAndUpload(ctx context.Context, logo string, ep *episode) error {
 		}
 	}()
 
-	l, err := loadPNG(logo)
+	l, err := loadPNG(*logo)
 	if err != nil {
-		return fmt.Errorf("could not load logo %s: %v", logo, err)
+		return fmt.Errorf("could not load logo %s: %v", *logo, err)
 	}
 
 	text := fmt.Sprintf("%d: %s", ep.Number, ep.Title)
